b8c31a52884ddd87a179886530231f836d929f8c
Alex <alexandersmith223@gmail.com>
2017-08-15 20:53:08 -0400

    Add user and stock relationship. Add stock list for user's portfolio.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,12 @@
 class Stock < ApplicationRecord
+
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+
   def self.find_by_ticker(ticker_symbol)
   	where(ticker: ticker_symbol).first
   end
-  
+
   def self.new_from_lookup(ticker_symbol)
     looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
     return nil unless looked_up_stock.name
@@ -14,10 +18,10 @@ class Stock < ApplicationRecord
 
   def price
   	closing_price = StockQuote::Stock.quote(ticker).close
-  	return "#{closing_price} (Closing)" if closing_price
+  	return "$#{closing_price} (Closing)" if closing_price
 
   	opening_price = StockQuote::Stock.quote(ticker).open
-  	return "#{opening_price} (Opening)" if opening_price
+  	return "$#{opening_price} (Opening)" if opening_price
   	'Unavailable'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :user_stocks, except: [:show, :edit, :update]
   devise_for :users
   get 'welcome/index'
   root 'welcome#index'
