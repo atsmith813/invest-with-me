f3b15e61e6a9b12e9fd646ac2f04c404df50c0b0
Alex <alexandersmith223@gmail.com>
2017-08-17 16:25:22 -0400

    Add add friend, show friend, and remove friend.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,31 @@ class UsersController < ApplicationController
 	def my_friends
 		@friendships = current_user.friends
 	end
+
+	def search
+		@users = User.search(params[:search_param])
+
+		if @users
+			@users = current_user.except_current_user(@users)
+			render partial: 'friends/lookup'
+		else
+			render status: :not_found, body: nil
+		end
+	end
+
+	def add_friend
+		@friend = User.find(params[:friend])
+		current_user.friendships.create(friend_id: @friend.id)
+
+		if current_user.save
+			redirect_to my_friends_path, notice: "Friend was successfully added!"
+		else
+			redirect_to my_friends_path, flash[:error] = "There was an error with adding this user as a friend."
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+		@user_stocks = @user.stocks
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'friendships/destroy'
+
   resources :user_stocks, except: [:show, :edit, :update]
   resources :users, only: [:show]
   resources :friendships
@@ -8,5 +10,7 @@ Rails.application.routes.draw do
   get 'my_portfolio' => 'users#my_portfolio'
   get 'search_stocks' => 'stocks#search'
   get 'my_friends' => 'users#my_friends'
+  get 'search_friends' => 'users#search'
+  post 'add_friend' => 'users#add_friend'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
