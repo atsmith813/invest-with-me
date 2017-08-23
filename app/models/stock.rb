1ab595b7bd41120f44bcd76575e0793a867afba7
Alex <alexandersmith223@gmail.com>
2017-08-23 11:37:13 -0400

    Fix price bug in Stock model.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -17,15 +17,11 @@ class Stock < ApplicationRecord
   end
 
   def price
-    if !last_price.blank?
-      return "$#{last_price} (Closing)"
-    else
-  	  closing_price = StockQuote::Stock.quote(ticker).close
-  	  return "$#{closing_price} (Closing)" if closing_price
+	  closing_price = StockQuote::Stock.quote(ticker).close
+	  return "$#{closing_price} (Closing)" if closing_price
 
-  	  opening_price = StockQuote::Stock.quote(ticker).open
-  	  return "$#{opening_price} (Opening)" if opening_price
-  	  'Unavailable'
-    end
+	  opening_price = StockQuote::Stock.quote(ticker).open
+	  return "$#{opening_price} (Opening)" if opening_price
+	  'Unavailable'
   end
 end
